1cce59c6d2921b9d0b606c709239d66454f4e15d
Eliaship <eliashi@metropolia.fi>
2020-10-27 12:16:41 +0200

    lab 5

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -2,7 +2,10 @@
 // This script manages a to-do list.
 
 // Need a global variable:
-var tasks = []; 
+var tasks = [];
+
+const button = document.getElementById('remove');
+button.addEventListener('click', removeDuplicates);
 
 // Function called when the form is submitted.
 // Function adds a task to the global array.
@@ -37,6 +40,21 @@ function addTask() {
     return false;
     
 } // End of addTask() function.
+function removeDuplicates() {
+    'use strict';
+    let message = '';
+    let output = document.getElementById('output');
+    tasks = tasks.filter(function(item, index, inputArray) {
+        output.innerHTML = message;
+        return inputArray.indexOf(item) == index;
+    });
+    message = '<h2>To-Do</h2><ol>';
+    for (let i = 0, count = tasks.length; i < count; i++) {
+        message += '<li>' + tasks[i] + '</li>';
+    }
+    message += '</ol>';
+    output.innerHTML = message;
+}
 
 // Initial setup:
 function init() {
