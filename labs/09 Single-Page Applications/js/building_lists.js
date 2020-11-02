0cc0959133323eda8f1bbd08ec8bc4789ee67fe7
Eliaship <eliashi@metropolia.fi>
2020-11-02 09:27:56 +0200

    lab 10 ja 9

--- labs/09 Single-Page Applications/js/building_lists.js
+++ labs/09 Single-Page Applications/js/building_lists.js
@@ -1,17 +1,37 @@
 
-var request = new XMLHttpRequest();
+let request = new XMLHttpRequest();
 request.open('GET', 'data/books.json', false);
 request.send(null);
-var data = JSON.parse(request.responseText);
+let data = JSON.parse(request.responseText);
 console.log(data);
 
-var books = data.books;
+let books = data.books;
 
-var list = document.createElement('ol');
-for (var i=0; i < books.length; i++) {
+let list = document.createElement('table');
+let heading1 = document.createElement('th');
+let heading2 = document.createElement('th');
+heading1.innerHTML = "name";
+heading2.innerHTML = "year";
+list.appendChild(heading1);
+list.appendChild(heading2);
+
+for (let i=0; i < books.length; i++) {
 	console.log(books[i].title);
-	var item = document.createElement('li');
-	item.innerHTML = books[i].title;
-	list.appendChild(item);
+	let row = document.createElement('tr');
+	row.setAttribute("id", ""+ i +"");
+	let item1 = document.createElement('td');
+	let item2 = document.createElement('td');
+	item1.innerHTML = books[i].title;
+	item2.innerHTML = books[i].year;
+	row.appendChild(item1);
+	row.appendChild(item2);
+	list.appendChild(row);
 }
+let heading = document.querySelector('h1');
+
+document.querySelector('t').onkeypress = function() {
+	document.getElementById('0').heading.innerHTML = books[2].title = heading;
+};
+
+
 document.body.appendChild(list);
--- labs/09 Single-Page Applications/js/simple_dom.js
+++ labs/09 Single-Page Applications/js/simple_dom.js
@@ -1,24 +1,38 @@
 //console.log('page loaded');
 
 console.log(document);
-
-document.getElementById('save').onclick = save;
-document.querySelector('#userForm input[type="email"]').onkeypress = function() {
-	console.log('updating email');
-	var email = document.querySelector('#userForm input[type="email"]').value;
-	document.querySelector('#summary p').innerHTML = email;
-};
-
-
-function save() {
+/*
+document.getElementById('save').onclick = function() {
 	console.log('save');
 	// get a DOM object representing a form field.
-	var name = document.querySelector('#userForm input[type="text"]');
+	let name = document.querySelector('#userForm input[type="text"]');
 	console.log(name);
 	document.querySelector('#summary h1').innerHTML = name.value;
-	var data = document.querySelectorAll('#userForm input');
+	let data = document.querySelectorAll('#userForm input');
 	console.log(data);
-	var paragraphs = document.querySelectorAll('#summary p');
+	let paragraphs = document.querySelectorAll('#summary p');
 	console.log('found '+paragraphs.length+' p tags');
-	paragraphs[1].innerHTML = 'Hello World!';
-}
+	let sposti = document.querySelector('#userForm input[type="email"]');
+	let password = document.querySelector('#userForm input[type="password"]');
+	paragraphs[1].innerHTML = password.value;
+	paragraphs[0].innerHTML = sposti.value;
+
+};
+*/
+document.querySelector('#userForm input[type="email"]').onkeypress = function() {
+	console.log('updating email');
+	let email = document.querySelector('#userForm input[type="email"]').value;
+	document.getElementById('email').innerHTML = email;
+};
+
+document.querySelector('#userForm input[type="text"]').onkeypress = function() {
+	console.log('updating text');
+	let name = document.querySelector('#userForm input[type="text"]').value;
+	document.querySelector('#summary h1').innerHTML = name;
+};
+
+document.querySelector('#userForm input[type="password"]').onkeypress = function() {
+	console.log('updating email');
+	let password = document.querySelector('#userForm input[type="password"]').value;
+	document.getElementById('pass').innerHTML = password;
+};
